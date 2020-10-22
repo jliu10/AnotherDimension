4fc5b14c95f821f68598e7b04dfd51cd0d02bd0e
jliu10 <jliu10@stuy.edu>
2020-10-22 15:11:42 -0400

    tested sumRows()

--- Tester.java
+++ Tester.java
@@ -1,11 +1,20 @@
+import java.util.Arrays;
+
 public class Tester{
   public static void main(String[] args){
     int[]arrEmpty=new int[0];
     int[]arrSingle=new int[1];
     int[]arr1={0,1,2,1,-5};
+    int[][]  A = { {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 }
+                 };
+    int[]  B  =   {  1, 3, 5 };
     System.out.println(ArrayOps.sum(arr1));
     System.out.println(ArrayOps.sum(arrEmpty));
     System.out.println(ArrayOps.largest(arr1));
     System.out.println(ArrayOps.largest(arrSingle));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+    System.out.println(ArrayOps.sum(B));
   }
 }
