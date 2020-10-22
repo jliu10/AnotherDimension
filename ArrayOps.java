9e5d20988fdc8ecf996421c7bb8bbcbd7faf7411
jliu10 <jliu10@stuy.edu>
2020-10-22 14:53:38 -0400

    completed largest()

--- ArrayOps.java
+++ ArrayOps.java
@@ -8,7 +8,9 @@ public class ArrayOps{
   }
   public static int largest(int[] arr){
     int max=arr[0];
-    
+    for(int i=0;i<arr.length;i++){
+      if (arr[i]>max) max=arr[i];
+    }
     return max;
   }
   public static int[] sumRows(int[][] matrix){
