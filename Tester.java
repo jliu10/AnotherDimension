bcfcdd64d789010fa0ee6962123ccb5f07ec8424
jliu10 <jliu10@stuy.edu>
2020-10-22 14:44:39 -0400

    tested sums() for 1D arrays

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester{
   public static void main(String[] args){
-
+    int[]arrEmpty=new int[0];
+    int[]arr1={0,1,2,1,-5};
+    System.out.println(ArrayOps.sum(arr1));
+    System.out.println(ArrayOps.sum(arrEmpty));
   }
 }
